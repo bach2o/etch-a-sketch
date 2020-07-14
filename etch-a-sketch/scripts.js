eddc0cf4d3f3b11b4fc21cf5ac1e94b9d7532f16
bach2o <bachchakmak@gmail.com>
2020-07-14 22:53:21 +0200

    update

--- etch-a-sketch/scripts.js
+++ etch-a-sketch/scripts.js
@@ -1,15 +1,52 @@
-function genDivs (v) {
-    var e = document.body;
-    for (var i = 0; i < v; i++) {
-        var row = document.createElement("div");
-        row.className = "row";
-        for (var x = 0; x < v; x++) {
-            var cell = document.createElement("div");
-            cell.className = "gridsquare";
-            cell.innerText = (i*v) + x;
-            row.appendChild(cell);
-        }
-        e.appendChild(row);
-    }
-    document.getElementById("code").innerText = e.innerHTML;
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    // cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+// var button = document.getElementsByClassName("button");
+// button
+function newGrid() {
+  var old_grid_items = document.getElementsByClassName("grid-item");  
+  var txt;
+  var rows = prompt("Please enter number of rows:");
+  // if (rows == null || rows == "") {
+  //   txt = "User cancelled the prompt.";
+  // } 
+  var columns = prompt("Please enter number of columns:");
+  while(old_grid_items.length > 0) {
+    old_grid_items[0].parentNode.removeChild(old_grid_items[0]);
+  }
+  makeRows(rows, columns);
 }
+
+let test = document.getElementById("container");
+test.addEventListener("mouseover", function(event) {
+  // var selected_grid = event.target; 
+  // var h = selected_grid.backgroundColor;  
+  // console.log(h);
+  // selected_grid.style.backgroundColor = "hsl(${h}, ${s}, ${l})";
+  // event.target.style.backgroundColor = 'hsl(0, 100%, 50%)';
+  if (event.target.style.backgroundColor) {    
+    var x = event.target.style.backgroundColor;
+    console.log(x);
+  } else {
+    event.target.style.backgroundColor = 'hsl(0, 100%, 50%)';
+    console.log('nothing');
+  }
+  
+});
+
+// var button = document.getElementsByClassName("grid-item");
+// function increaseBlack(red, green, blue) {
+//   button.style
+
+// }
+
+// button.hover()
