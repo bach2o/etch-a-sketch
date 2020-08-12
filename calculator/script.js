88ff745afb92b045ea79c9f415855d45303b44b6
bach2o <bachchakmak@gmail.com>
2020-08-12 13:43:46 +0200

    calculator

---
+++ calculator/script.js
@@ -0,0 +1,40 @@
+function add (a, b) {
+	return a + b;
+}
+
+function subtract (a, b) {
+	return a - b;
+}
+
+function sum (array) {
+	let arrayLength = array.length;
+	let sum = 0;
+	for (i = 0; i < arrayLength; i++) {
+		sum += array[i];
+	}
+	return sum;
+}
+
+function multiply (array) {	
+	let product = 1;
+	for (i = 0; i < array.length; i++) {
+		product *= array[i];
+	}
+	return product;
+}
+
+function power(base, power) {
+	let product = 1;
+	for (i = 0; i < power; i++) {
+		product *= base;
+	}
+	return product;
+}
+
+function factorial(n) {
+	if (n === 0) {
+		return 1;
+	} else {
+		return n * factorial(n - 1);
+	}
+}
